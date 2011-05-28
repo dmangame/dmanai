35b5b2cd87597bca196b7bc60196c5edcf7492d9
okay zed <okay.zed@gmail.com>
2011-05-27 18:07:48 -0700

    Add an AI with file dependencies.

---
+++ okay/remote_dep.py
@@ -0,0 +1,19 @@
+# Test that remote dependency / local dependency loading is working.
+import ai
+import random
+import itertools
+from operator import attrgetter
+from collections import defaultdict
+import sys
+import os
+
+# This loads the module 'okay' from filesystem or github, depending on how
+# this module is loaded.
+require_dependency('okay')
+
+AIClass = "RushAI"
+EXPLORER_RATIO=4
+
+# It kind of just sits there.
+class RemoteDepAI(okay.OkayAI):
+  pass
